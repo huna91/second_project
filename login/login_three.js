a881050718f058e3ec643bc532064251da82f91e
WooJonghwa <wjh423@gmail.com>
2022-08-22 09:30:42 +0900

    로그인페이지

---
+++ login/login_three.js
@@ -0,0 +1,139 @@
+import * as THREE from 'three';
+import { GLTFLoader } from '/jsm/loaders/GLTFLoader.js'
+import { OrbitControls } from '/jsm/controls/OrbitControls.js'
+
+class App {
+    constructor() {
+        const divContainer = document.querySelector("#webgl-container");
+        this._divContainer = divContainer;
+
+        const renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setPixelRatio(window.devicePixelRatio);
+        divContainer.appendChild(renderer.domElement);
+
+        this._renderer = renderer;
+
+        const scene = new THREE.Scene();
+        // 배경색 바꾸는 코드
+        //scene.background = new THREE.Color(0xffffff);
+        this._scene = scene;
+
+        this._setupCamera();
+        this._setupLight();
+        this._setupModel();
+        this._setupControls();
+
+        window.onresize = this.resize.bind(this);
+        this.resize();
+
+        requestAnimationFrame(this.render.bind(this));
+    }
+
+    _setupControls() {
+        new OrbitControls(this._camera, this._divContainer);
+    }
+
+    _setupCamera() {
+        const width = this._divContainer.clientWidth/2; //전체화면 넓이의 절반
+        const height = this._divContainer.clientHeight;
+        const camera = new THREE.PerspectiveCamera(
+            60,
+            width / height,
+            1,
+            500
+        );
+        camera.position.set(4, 8, 10);
+        this._camera = camera;
+    }
+
+    _setupLight() {
+        const color = 0xffffff;  //광원의 색상
+        const intensity = 1.5;  //광원의 세기
+        const light = new THREE.DirectionalLight(color, intensity);  //색상과 세기로 광원 생성
+        light.position.set(0, 1, 1); //광원의 위치
+        this._scene.add(light);
+    }
+
+    changeAnimation(animationName) {
+        const previousAnimationAction = this._currentAnimationAction;
+        this._currentAnimationAction = this._animationsMap[animationName];
+
+        if(previousAnimationAction !== this._currentAnimationAction) {
+            previousAnimationAction.fadeOut(0.5);
+            this._currentAnimationAction.reset().fadeIn(0.5).play();
+        }
+    }
+
+    _setupAnimations(gltf) {
+        const model = gltf.scene;
+        const mixer = new THREE.AnimationMixer(model);
+        const gltfAnimations = gltf.animations;
+        const domControls = document.querySelector("#controls");
+        const animationsMap = {};
+
+        gltfAnimations.forEach(animationClip => {
+            const name = animationClip.name;
+            //console.log(name); // 행동목록 콘솔창에 출력
+
+            const domButton = document.createElement("div");
+            domButton.classList.add("button");
+            //domButton.innerText = name;
+            domControls.appendChild(domButton);
+
+            domButton.addEventListener("click", () => {
+                const animationName = domButton.innerHTML;
+                this.changeAnimation(animationName);
+            });
+
+            const animationAction = mixer.clipAction(animationClip);
+            animationsMap[name] = animationAction;
+        });
+
+        this._mixer = mixer;
+        this._animationsMap = animationsMap;
+        this._currentAnimationAction = this._animationsMap["Walking"];
+        this._currentAnimationAction.play();
+    }
+
+    _setupModel() {
+        new GLTFLoader().load("./data/Robot.glb", (gltf) => {
+            const model = gltf.scene;
+            this._scene.add(model);
+
+            this._setupAnimations(gltf);
+        });
+    }
+
+    resize() {
+        const width = this._divContainer.clientWidth/2; //전체화면의 넓이의 절반
+        const height = this._divContainer.clientHeight;
+
+        this._camera.aspect = width / height;
+        this._camera.updateProjectionMatrix();
+
+        this._renderer.setSize(width, height);
+    }
+
+    render(time) {
+        this._renderer.render(this._scene, this._camera);
+        this.update(time);
+        requestAnimationFrame(this.render.bind(this));
+    }
+
+    update(time) {
+        time *= 0.001;
+
+        if(this._mixer) {
+            const deltaTime = time - this._previousTime;
+            this._mixer.update(deltaTime);
+        }
+
+        this._previousTime = time;
+
+        //this._currentAnimationAction.rotation.y = time;
+    }
+}
+
+window.onload = function() {
+    new App();
+}
